4f87f1427255f5a009bae15e2dff5804e5ecab58
cis101 <cis101@secs.iub.edu.bd>
2017-10-04 09:20:15 +0600

    Assignment6

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,10 @@
 //JS code goes here
+var turn = false;
+function play(event) {
+  console.log(event);
+  event.target.innerHTML = turn? "O" : "X";
+  turn = !turn;
+}
+
+//var cell0 = document.getElementById("0");
+//cell0.addEventListener('click',play);
